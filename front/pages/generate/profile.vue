52a77e064393084887ff1581039019b92bee2fab
rere <regis-graptin@sfr.fr>
2023-02-25 18:36:35 +0100

    feat: create basic page for showing the user profile

---
+++ front/pages/generate/profile.vue
@@ -0,0 +1,21 @@
+<template>
+
+    
+
+    <section>
+
+        <h2>Create a new profile</h2>
+
+        <ul>
+            <li>Profile 1</li>
+            <li>Profile 2</li>
+        </ul>
+
+    </section>
+
+</template>
+<script>
+    export default {
+        middleware: 'auth',
+    }
+</script>
