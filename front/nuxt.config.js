109ce1765d74edd68f2fde28c3f8f613ea645631
rere <regis-graptin@sfr.fr>
2023-02-25 17:16:35 +0100

    fix: upgrade nuxt component build error

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -118,6 +118,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: [
+      'defu'
+    ],
     postcss: {
       plugins: {
         tailwindcss: {},
