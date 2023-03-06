c4a27a0a2110aad02def07f3a6d4bcee18da1bef
rere <regis-graptin@sfr.fr>
2023-03-06 21:53:38 +0100

    feat: front - add budget field

--- front/components/profile/components/FirstStep.vue
+++ front/components/profile/components/FirstStep.vue
@@ -49,6 +49,48 @@
 
         </article>
 
+        <br /><br /><br />
+
+        <h3 class="mb-5 text-lg font-medium text-gray-900">Cost</h3>
+        <article class="mt-5 flex items-center justify-around w-1/2 mx-auto">
+            
+            
+            <ul class="grid w-full gap-6 md:grid-cols-3">
+                <li>
+                    <input v-model="profile.cost" type="radio" id="budget-small" name="budget" value="0" class="hidden peer" required>
+                    <label for="budget-small" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">                           
+                        <div class="block">
+                            <div class="w-full text-lg font-semibold">0-50 MB</div>
+                            <div class="w-full">Small budget</div>
+                        </div>
+                        <svg aria-hidden="true" class="w-6 h-6 ml-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    </label>
+                </li>
+                <li>
+                    <input v-model="profile.cost" type="radio" id="budget-medium" name="budget" value="3" class="hidden peer">
+                    <label for="budget-medium" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <div class="block">
+                            <div class="w-full text-lg font-semibold">0-50 MB</div>
+                            <div class="w-full">Medium</div>
+                        </div>
+                        <svg aria-hidden="true" class="w-6 h-6 ml-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    </label>
+                </li>
+                <li>
+                    <input v-model="profile.cost" type="radio" id="budget-big" name="budget" value="5" class="hidden peer">
+                    <label for="budget-big" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <div class="block">
+                            <div class="w-full text-lg font-semibold">500-1000 MB</div>
+                            <div class="w-full">High budget</div>
+                        </div>
+                        <svg aria-hidden="true" class="w-6 h-6 ml-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    </label>
+                </li>
+            </ul>
+
+        </article>
+
+
     </section>
 </template>
 
